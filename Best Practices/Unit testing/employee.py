5d8e9a9883abf606d5fa8a897d7b16322468a072
Renan Nunes Steinck <renan.alonkin@gmail.com>
2020-10-20 21:11:43 -0300

    Finishing Unit Test class
    Here we saw unit testing and mocking using unittest from python.

---
+++ Best Practices/Unit testing/employee.py
@@ -0,0 +1,27 @@
+import requests
+
+class Employee:
+    raise_amount = 1.05
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+
+    @property
+    def fullname(self):
+        return '{0} {1}'.format(self.first, self.last)
+
+    @property
+    def email(self):
+        return '{0}.{1}@company.com'.format(self.first, self.last).lower()
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
+
+    def monthly_schedule(self, month):
+        response = requests.get(f'http://company.com/{self.last}/{month}')
+        if response.ok:
+            return response.text
+        else:
+            return 'Bad Response!'
